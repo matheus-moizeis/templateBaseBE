24792e071cc7a2773e3f91910e0b3a894aa01c50
Matheus Moizeis <matheusmoizeis@outlook.com>
2025-04-09 23:20:56 -0300

    refatoração do projeto e configuração inicial de camada de infra

--- src/TBB.Domain/Enum/StatusCardEnum.cs
+++
@@ -1,9 +0,0 @@
-namespace TBB.Domain.Enum;
-
-public enum StatusCardEnum
-{
-    Todo = 1,
-    Done = 2,
-    Late = 3,
-    NoStatus = 4,
-}
--- src/TBB.Domain/Enum/StatusItemEnum.cs
+++
@@ -1,7 +0,0 @@
-namespace TBB.Domain.Enum;
-
-public enum StatusItemEnum
-{
-    Active = 1,
-    Filled = 2,
-}
